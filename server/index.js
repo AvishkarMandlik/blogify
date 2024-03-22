cd78c0e561cbc72868cad699daf41d6bc019c528
AvishkarMandlik <mandlikavi121@gmail.com>
2024-03-22 21:27:05 +0530

    for frontend and backend path or middleware done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,20 @@ const mongoConnection = require('./mongoConn');
 
 const app = express();
 const PORT = 5000;
+const path = require('path');
+app.use(express.json());
+
+
+
+
+
+
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+});
 
 mongoConnection.connect()
   .then(() => {
