cf3bdc6a699d1eedf6eb4c0639ef55a3d964b8c7
AvishkarMandlik <mandlikavi121@gmail.com>
2024-03-23 17:13:26 +0530

    signup api working proper

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const mongoConnection = require('./mongoConn');
-
+const bcrypt = require('bcryptjs');
 const app = express();
 const PORT = 5000;
 const path = require('path');
 app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+  const { username,email, password,role } = req.body;
+  const usersCollection = mongoConnection.getCollection('users');
+
+  const existingUser = await usersCollection.findOne({ email});
+  if (existingUser) {
+    return res.json({
+      success: false,
+      message: "Email already exists"
+  })
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await usersCollection.insertOne({ username,email, role, password: hashedPassword });
+  const savedUser =  { username,email, role, password: hashedPassword }
+    res.json({
+      success: true,
+      message: "User created successfully",
+      data: savedUser
+  })});
+
+
 
 
 
